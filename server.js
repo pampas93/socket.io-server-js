4d334bd6d53b7e9349320a31a62562157ffe081e
pampas93 <abhijit.93@hotmail.com>
2019-03-30 11:56:54 -0600

    Setup simple socket.io

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 var express = require('express');
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 var app = express();
 
 app.use(express.static(__dirname));
 var server = app.listen(3000, () => {
     console.log('server is running on ', server.address().port);
+});
+
+io.on('connection', () => {
+    console.log('A user is connected');
 });
