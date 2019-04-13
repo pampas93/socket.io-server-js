cb840d6cc5cba8da47e92771ad9ec0fb1bbdb483
pampas93 <abhijit.93@hotmail.com>
2019-04-13 08:21:19 -0600

    Refactor the server to create simple node without express + with socket.io

--- server.js
+++ server.js
@@ -1,14 +1,32 @@
-var express = require('express');
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var http = require('http'),
+    fs = require('fs'),
+    // NEVER use a Sync function except at start-up!
+    index = fs.readFileSync(__dirname + '/index.html');
 
-var app = express();
-
-app.use(express.static(__dirname));
-var server = app.listen(3000, () => {
-    console.log('server is running on ', server.address().port);
+// Send index.html to all requests
+var app = http.createServer(function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end(index);
 });
 
-io.on('connection', () => {
-    console.log('A user is connected');
+// Socket.io server listens to our app
+var io = require('socket.io').listen(app);
+
+// Send current time to all connected clients
+function sendTime() {
+    io.emit('time', { time: new Date().toJSON() });
+}
+
+// Send current time every 10 secs
+setInterval(sendTime, 10000);
+
+// Emit welcome message on connection
+io.on('connection', function(socket) {
+    // Use socket to communicate with this particular client only, sending it it's own id
+    console.log("New client joined");
+    socket.emit('welcome', { message: 'Welcome!', id: socket.id });
+
+    socket.on('i am client', console.log);
 });
+
+app.listen(3000);
